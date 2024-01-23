04ece7e841e103a8ec1efe7f539295e8f7015604
Mitsuki <miyagimitsuki39@gmail.com>
2024-01-23 15:27:39 +0800

    whatsapp ui

---
+++ src/pages/splash/index.tsx
@@ -0,0 +1,34 @@
+import {
+  Container,
+  EncryptionIcon,
+  Link,
+  Logo,
+  LogoWrapper,
+  Progress,
+  SubTitle,
+  Title,
+} from "./styles";
+
+type SplashPageProps = {
+  progress: number;
+};
+
+export default function SplashPage(props: SplashPageProps) {
+  const { progress } = props;
+
+  return (
+    <Container>
+      <LogoWrapper>
+        <Logo id="whatsapp" />
+      </LogoWrapper>
+      <Progress progess={progress} />
+      <Title>WhatsApp</Title>
+      <SubTitle>
+        <EncryptionIcon id="lock" /> End-to-end encrypted. Built by{" "}
+        <Link href="https://github.com/abdurafeyf" target="_blank">
+          Abdul Rafey
+        </Link>{" "}
+      </SubTitle>
+    </Container>
+  );
+}
